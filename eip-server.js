3e27b84eecd1f8229e8d0b78c7727cf61a4cc576
Sylvain Garant <xobtah@vps372804.ovh.net>
2017-11-19 17:21:34 +0100

    New routes

--- eip-server.js
+++ eip-server.js
@@ -6,11 +6,48 @@
 let SportsFun = require('./server/SportsFun');
 let Server = new SportsFun.Server();
 
-Server.Init(function (DataBase) {
+Server.Init((DataBase) => {
     console.log('Server started!');
 
     Server.Route('get', '/', (req, res) => {
         console.log('User gets the root');
         res.sendStatus(200);
+    });
+
+    Server.Route('post', '/hardware', (req, res) => {
+	res.sendStatus(200);
+    });
+
+    Server.Route('get', '/api/users', (req, res) => {
+	DataBase.Users.Find().toArray((err, result) => {
+	    res.header("Access-Control-Allow-Origin", "*");
+	    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	    if (err)
+		res.sendStatus(500);
+	    else
+		res.send(result);
+	});
+    });
+
+    Server.Route('get', '/api/posts', (req, res) => {
+	DataBase.Posts.Find().toArray((err, result) => {
+	    res.header("Access-Control-Allow-Origin", "*");
+	    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	    if (err)
+		res.sendStatus(500);
+	    else
+		res.send(result);
+	});
+    });
+
+    Server.Route('get', '/api/activities', (req, res) => {
+	DataBase.Activities.Find().toArray((err, result) => {
+	    res.header("Access-Control-Allow-Origin", "*");
+	    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+	    if (err)
+		res.sendStatus(500);
+	    else
+		res.send(result);
+	});
     });
 });
