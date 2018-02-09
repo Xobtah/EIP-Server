64585328c4b19774c9f8797600f87c93c963dee2
Sylvain Garant <sylvain.garant@epitech.eu>
2018-02-09 17:47:50 -0500

    Mongoose connection error handling

--- eip-server.js
+++ eip-server.js
@@ -12,16 +12,18 @@ let Morgan = require('morgan');
 
 let mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/sportsfun');
+mongoose.connect('mongodb://localhost:27017/sportsfun').then(() => {
+    app.use(Morgan('combined'));
+    app.use(bodyParser.json({ limit: '50mb' }));
+    app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 
-app.use(Morgan('combined'));
-app.use(bodyParser.json({ limit: '50mb' }));
-app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+    require('./models/User.js');
 
-require('./models/User.js');
+    app.use(require('./routes'));
 
-app.use(require('./routes'));
-
-//httpServer = require('http').createServer(app);
-app.listen(8080, () => console.log('App is listening! :)'));
-//httpServer.listen(8080, () => console.log('App is listening! :)'));
+    //httpServer = require('http').createServer(app);
+    app.listen(8080, () => console.log('App is listening! :)'));
+    //httpServer.listen(8080, () => console.log('App is listening! :)'));
+}).catch((err) => {
+    console.log(err);
+});
