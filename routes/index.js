3c97d3d4cc80e7fa6dcf650bc067d64b00c4ee36
Sylvain Garant <101010lna42@gmail.com>
2018-02-15 18:39:46 -0500

    Access control origin

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,12 @@ let router = express.Router();
 
 router.use('/static', express.static(path.join(__dirname, '..', 'assets')));
 
+router.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+});
+
 router.use('/api', require('./api'));
 
 // Error middleware
