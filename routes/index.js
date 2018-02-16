ca2e696326c15daa80e3cfae02b6f84ae9ca6e7f
Sylvain Garant <101010lna42@gmail.com>
2018-02-15 20:11:21 -0500

    Elsa

--- routes/index.js
+++ routes/index.js
@@ -10,8 +10,10 @@ let router = express.Router();
 router.use('/static', express.static(path.join(__dirname, '..', 'assets')));
 
 router.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, token");
     next();
 });
 
