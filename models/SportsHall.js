b999bd45adee6ba21d0d25fba1e6ca2d85b898ee
Sylvain Garant <101010lna42@gmail.com>
2018-03-18 11:55:39 -0400

    SportsHall model

---
+++ models/SportsHall.js
@@ -0,0 +1,12 @@
+/*
+** Author: Sylvain Garant
+** Website: https://github.com/Xobtah
+*/
+
+let mongoose = require('mongoose');
+
+let SportsHallSchema = mongoose.Schema({
+    
+});
+
+mongoose.model('SportsHall', SportsHallSchema);
