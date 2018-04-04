e5a5ad1a8e9607cdb7833af673dd1c40beca7fa7
Sylvain Garant <101010lna42@gmail.com>
2018-04-04 14:03:23 -0400

    Coach activity management routes

--- routes/api/coaches.js
+++ routes/api/coaches.js
@@ -8,12 +8,59 @@ let mid = require('./../middlewares');
 
 // Plus users routes
 
-router.post('/edit/username', mid.checkLogin, mid.fields([ 'username' ]), (req, res) => {
-    req.user.username = req.fields.username;
+// router.post('/edit/username', mid.checkLogin, mid.fields([ 'username' ]), (req, res) => {
+//     req.user.username = req.fields.username;
+//     req.user.save((err) => {
+//         if (err)
+//             return (res.status(500).send({ success: false, message: err }));
+//         res.status(200).send({ success: true, message: 'Username changed' });
+//     });
+// });
+
+/**
+* @api {POST} /api/coach/activities Add activities to a coach
+* @apiName AddCoachActivities
+* @apiGroup Coach
+*
+* @apiParam {[ ID ]} activities An array of activities ID;
+*
+* @apiSuccess {Boolean} success True
+* @apiSuccess {String} message Success message.
+*/
+
+router.post('/activities', mid.checkUser, mid.fields([ 'activities' ]), (req, res) => {
+    req.fields.activities.forEach((act) => {
+        if (req.user.activities.indexOf(act) < 0)
+            req.user.activities.push(act);
+    });
     req.user.save((err) => {
         if (err)
-            return (res.status(500).send({ success: false, message: err }));
-        res.status(200).send({ success: true, message: 'Username changed' });
+            return (res.status(500).send({ success: true, message: err }));
+        res.status(200).send({ success: true, message: 'OK' });
+    });
+});
+
+/**
+* @api {DELETE} /api/coach/activities Remove activities from a coach
+* @apiName DeleteCoachActivities
+* @apiGroup Coach
+*
+* @apiParam {[ ID ]} activities An array of activities ID;
+*
+* @apiSuccess {Boolean} success True
+* @apiSuccess {String} message Success message.
+*/
+
+router.delete('/activities', mid.checkUser, mid.fields([ 'activities' ]), (req, res) => {
+    req.fields.activities.forEach((act) => {
+        let index = req.user.activities.indexOf(act);
+        if (index > -1)
+            req.user.activities.splice(index, 1);
+    });
+    req.user.save((err) => {
+        if (err)
+            return (res.status(500).send({ success: true, message: err }));
+        res.status(200).send({ success: true, message: 'OK' });
     });
 });
 
