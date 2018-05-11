95be06b1e3090a7edd0f25fc6735f5c423b877cd
Sylvain Garant <101010lna42@gmail.com>
2018-05-11 07:38:06 -0400

    Adding 'module' field to the data

--- websocket/data.js
+++ websocket/data.js
@@ -8,6 +8,6 @@ module.exports = function (data, links, socket) {
         return ;
     links.get(data.link_id).forEach((link) => {
         if (link.socket !== socket)
-            link.socket.emit('data', { link_id: data.link_id, body: { value: data.body.value } });
+            link.socket.emit('data', { link_id: data.link_id, module: data.module, body: { value: data.body.value } });
     });
 };
