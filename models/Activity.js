676e06f33e24a5e8f399d820341ea0f1acbe59df
Sylvain Garant <101010lna42@gmail.com>
2018-05-23 13:46:38 +0200

    .

--- models/Activity.js
+++ models/Activity.js
@@ -11,7 +11,7 @@ let ActivitySchema = mongoose.Schema({
         required: true
     },
     game: {
-        type: mongoose.Schema.Types.ObjectId,
+        type: Number,
         required: true
     },
     type: {
