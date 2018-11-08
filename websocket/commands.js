ecd74383ad6c04b150fb2b6fc05d39a2a0fbc917
Sylvain Garant <101010lna42@gmail.com>
2018-11-08 11:44:13 +0100

    Merge

--- websocket/commands.js
+++ websocket/commands.js
@@ -4,7 +4,7 @@
 */
 
 module.exports = {
-    link (data, links, socket) {
+    link(data, links, socket) {
         socket.link_id = data.link_id;
         if (links.has(data.link_id)) {
             links.get(data.link_id).push({ type: data.body.type, socket });
@@ -15,25 +15,30 @@ module.exports = {
         }
         else
             links.set(data.link_id, [ { type: data.body.type, socket } ]);
-        console.log('Link: ' + data.link_id);
-	socket.emit('info', 'Linked with id \'' + data.link_id + '\'');
+        socket.emit('info', 'Linked with id \'' + data.link_id + '\'');
     },
-    start_game (data, links, socket) {
-        if (links.has(data.link_id))
+    start_game(data, links, socket) {
+        if (links.has(data.link_id)) {
             links.get(data.link_id).forEach((link) => {
                 if (link.socket !== socket)
                     link.socket.emit('command', { link_id: data.link_id, body: { command: 'start_game' } });
             });
-        console.log(data.link_id + ': Game started');
-	socket.emit('info', 'Game started');
+            console.log(data.link_id + ': Game started');
+            socket.emit('info', 'Game started');
+        }
+        else
+            socket.emit('info', 'Link before you start a game');
     },
-    end_game (data, links, socket) {
-        if (links.has(data.link_id))
+    end_game(data, links, socket) {
+        if (links.has(data.link_id)) {
             links.get(data.link_id).forEach((link) => {
                 if (link.socket !== socket)
                     link.socket.emit('command', { link_id: data.link_id, body: { command: 'end_game' } });
             });
-        console.log(data.link_id + ': Game ended');
-	socket.emit('info', 'Game ended');
+            console.log(data.link_id + ': Game ended');
+            socket.emit('info', 'Game ended');
+        }
+        else
+            socket.emit('info', 'Link before you end a game');
     }
 };
