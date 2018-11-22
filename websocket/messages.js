d6defe212d5df2de8c1928a24b47033ab5ce4fd3
Sylvain Garant <101010lna42@gmail.com>
2018-11-22 16:19:21 +0100

    Logging connected users

--- websocket/messages.js
+++ websocket/messages.js
@@ -25,7 +25,6 @@ module.exports.registerMessages = function (socket, data) {
     connectedUsers.set(token._id.toString(), socket);
     socket.userId = token._id.toString();
 
-    console.log('Destinataire: ' + message.to + ' (' + typeof message.to.toString() + ')');
     console.log('Connected users: ' + JSON.stringify(connectedUsers));
 
     socket.emit('registerMessages', { message: 'OK' });
